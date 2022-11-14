8e3c7175086d8c51c31719679491e5460247f4a7
Logan Joecks <ljoecks@gmail.com>
2022-11-14 14:27:59 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'pizza.party',
+  appName: 'pizza-party',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
